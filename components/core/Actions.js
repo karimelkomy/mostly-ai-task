fefb3a5363e796ff4a07ce21f0513ca687ce4a33
karimelkomy <karimelkomy@msn.com>
2023-08-03 21:10:31 +0300

    Add core actions file

---
+++ components/core/Actions.js
@@ -0,0 +1,44 @@
+import { t } from "testcafe";
+import {
+  selectorXP,
+  getCurrentUrl,
+  validateElementVisibility,
+} from "../../utilities/helpers";
+
+export default class Actions {
+  async click(elementXP) {
+    try {
+      await this.validateElementVisibility(elementXP);
+
+      await t.click(selectorXP(elementXP));
+    } catch (e) {
+      throw new Error(`
+		click failed - "${elementXP}" element does not exist.
+		Current URL: ${await getCurrentUrl()}
+	    `);
+    }
+  }
+
+  async hover(elementXP) {
+    await this.validateElementVisibility(elementXP);
+    await t.hover(selectorXP(elementXP));
+  }
+
+  async clear(elementXP) {
+    await this.click(elementXP);
+    await t.pressKey("ctrl+a delete");
+  }
+
+  async fill(elementXP, value) {
+    await this.clear(elementXP);
+    await t.typeText(selectorXP(elementXP), value.toString());
+  }
+
+  async pressEnter() {
+    await t.pressKey("enter");
+  }
+
+  async validateElementVisibility(elementXP) {
+    await validateElementVisibility(elementXP);
+  }
+}
